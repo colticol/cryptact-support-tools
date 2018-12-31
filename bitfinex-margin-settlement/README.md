63b0bde5d08843345b91ba313376d0451012ce11
colticol <colticol@gmail.com>
2018-12-31 21:14:21 +0900

    typo

--- bitfinex-margin-settlement/README.md
+++ bitfinex-margin-settlement/README.md
@@ -1,6 +1,6 @@
 How to Use
 ==================
 
-1. BitfinexのReportsより，Ledersの履歴をダウンロードする，
+1. BitfinexのReportsより，Ledgersの履歴をダウンロードする，
 2. `python3 main.py csvファイル`を実行する．
 3. `custom.csv` が出力される．
--- common/custom.py
+++ common/custom.py
@@ -8,6 +8,7 @@ class CustomFile(object):
   def __init__(self):
     self.t = pd.DataFrame(columns=['Timestamp', 'Action', 'Source', 'Base', 'DerivType', 'DerivDetails', 'Volume', 'Price', 'Counter', 'Fee', 'FeeCcy', 'Comment'])
 
+
   def append(self, *args):
     row = pd.Series(args, index=self.t.columns)
     self.t = self.t.append(row, ignore_index=True)
